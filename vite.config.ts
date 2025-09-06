3122634416b5a8bf256269bef4ac4ab4b30028fa
James Michael Hunter Turner <michael@turneratlanta.com>
2025-09-05 22:58:24 -0400

    Update vite.config.ts
    Signed-off-by: James Michael Hunter Turner <michael@turneratlanta.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-build: {
-  outDir: 'dist',
-  sourcemap: true,
-  rollupOptions: {
-    input: 'src/worker.js', // adjust path as needed
-    output: {
-      entryFileNames: 'worker.mjs',
-      format: 'es',
-    },
-    manualChunks: {
-      react: ["react", "react-dom", "react-router-dom"],
-      chartjs: ["chart.js"],
-      capture: ["html2canvas"]
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 5173 },
+  build: {
+      outDir: 'dist',
+      sourcemap: true,
+      rollupOptions: {
+        input: 'src/worker.js',
+        output: {
+          entryFileNames: 'worker.mjs',
+          format: 'es'
+        },
+        manualChunks: {
+          react: ["react", "react-dom",          
+          "react-router-dom"],
+          chartjs: ["chart.js"],
+          capture: ["html2canvas"]
+        } 
+      }
     }
   }
-}
+});
