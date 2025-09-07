3754e70204877019093d26099fc6f3682b8e19ef
James Michael Hunter Turner <michael@turneratlanta.com>
2025-09-07 18:09:01 -0400

    Update root.ts
    Signed-off-by: James Michael Hunter Turner <michael@turneratlanta.com>

--- my-react-router-app/root.ts
+++ my-react-router-app/root.ts
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Root from './Root';
-import App from './App';
-import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Root>
-      <App />
-    </Root>
-  </React.StrictMode>
-);
+type RootProps = {
+  children: React.ReactNode;
+};
+
+export default function Root({ children }: RootProps) {
+  return (
+    <div className="root-container">
+      {/* You can add global nav, header, footer here later */}
+      {children}
+    </div>
+  );
+}
