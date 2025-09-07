fc89e1fb2adc416d0e5dc65ab8f1ee33a35e2c4a
James Michael Hunter Turner <michael@turneratlanta.com>
2025-09-07 19:02:03 -0400

    Update worker.ts
    Signed-off-by: James Michael Hunter Turner <michael@turneratlanta.com>

--- src/worker.ts
+++ src/worker.ts
@@ -1,1 +1,10 @@
+export default {
+  async fetch(request: Request): Promise<Response> {
+    // Basic response for all requests
+    return new Response('Insight Hunter Worker is running!', {
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  },
+};
+
 
