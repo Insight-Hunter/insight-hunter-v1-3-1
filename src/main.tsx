0f1e0af227dc71fb850a54ad79336be9974256f1
James Michael Hunter Turner <michael@turneratlanta.com>
2025-09-07 18:12:54 -0400

    Update main.tsx
    Signed-off-by: James Michael Hunter Turner <michael@turneratlanta.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
-// src/main.tsx
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./components/welcome.css";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Root from './Root';
+import App from './App';
+import './styles.css';
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element #root not found");
-
-createRoot(root).render(
-   <React.StrictMode>
-     <BrowserRouter>
-       <App />
-     </BrowserRouter>
-   </React.StrictMode>
-  );
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Root>
+      <App />
+    </Root>
+  </React.StrictMode>
+);
